6ff23fea1c4ac1f517d4502218603332bccc6323
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-20 10:34:07 -0600

    Add backend structure for Products

---
+++ app/api/products.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, request
+from flask_login import login_required, current_user, login_user, logout_user
+from app.forms import SignupForm, validation_errors_formatter
+from app.models import db, User
+
+
+bp = Blueprint("products", __name__, url_prefix="/products")
+
+
+@bp.route("",  methods=["POST"])
+def post_product():
+    return "post_product"
+
+
+@bp.route("",  methods=["GET"])
+def get_products():
+    return "get_products"
+
+
+@bp.route("<int:product_id>",  methods=["GET"])
+def get_product(product_id):
+    return "get_products"
+
+
+@bp.route("<int:product_id>",  methods=["PUT"])
+def put_product(product_id):
+    return "put_product"
+
+
+@bp.route("<int:product_id>",  methods=["DELETE"])
+def delete_product(product_id):
+    return "delete_product"
