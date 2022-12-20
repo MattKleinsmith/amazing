ed793e1e0df91588d1a6a95e92acd59e9e8051a8
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-20 11:18:16 -0600

    Product backend CRUD

--- app/api/products.py
+++ app/api/products.py
@@ -1,31 +1,72 @@
 from flask import Blueprint, request
 from flask_login import login_required, current_user
 from app.models import db, Product
-
+from app.forms import ProductForm, validation_errors_formatter
+from sqlalchemy.exc import IntegrityError
 
 bp = Blueprint("products", __name__, url_prefix="/products")
 
 
 @bp.route("",  methods=["POST"])
+@login_required
 def post_product():
-    return "post_product"
+    form = ProductForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if (form.validate_on_submit()):
+        product = Product(
+            seller=current_user,
+            title=form.title.data,
+            price=form.price.data,
+            description=form.description.data
+        )
+        db.session.add(product)
+        db.session.commit()
+        return product.to_dict(), 201
+    return {'errors': validation_errors_formatter(form, form.errors)}, 400
 
 
 @bp.route("",  methods=["GET"])
 def get_products():
-    return "get_products"
+    return [product.to_dict() for product in Product.query]
 
 
 @bp.route("<int:product_id>",  methods=["GET"])
 def get_product(product_id):
-    return "get_products"
+    try:
+        product = Product.query.filter(Product.id == product_id).first()
+        return product.to_dict() if product else (f"Product with id {product_id} not found", 404)
+    except Exception:
+        return "500", 500
 
 
 @bp.route("<int:product_id>",  methods=["PUT"])
 def put_product(product_id):
-    return "put_product"
+    product = Product.query.filter(Product.id == product_id,
+                                   Product.seller_id == current_user.id).first()
+    if (not product):
+        return "404", 404
+    form = ProductForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        product.title = form.title.data
+        product.price = form.price.data
+        product.description = form.description.data
+        db.session.commit()
+        return product.to_dict()
+    return {'errors': validation_errors_formatter(form, form.errors)}, 400
 
 
 @bp.route("<int:product_id>",  methods=["DELETE"])
 def delete_product(product_id):
-    return "delete_product"
+    try:
+        product = db.session.query(Product).filter(
+            Product.id == product_id, Product.seller_id == current_user.id).first()
+        if (product):
+            db.session.delete(product)
+            db.session.commit()
+            return {"message": f"Deleted product with id {product_id}"}
+        return "404", 404
+    except IntegrityError as e:
+        return {"errors": {
+            "server": "Server failed to delete"
+        }}, 500
--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SubmitField, EmailField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms.fields import StringField, SubmitField, EmailField, PasswordField, FloatField, TextAreaField
+from wtforms.validators import DataRequired, Email, NumberRange, Length
 
 
 def validation_errors_formatter(form, validation_errors):
@@ -25,4 +25,12 @@ class SignupForm(FlaskForm):
     fullname = StringField("Full name", validators=[DataRequired()])
     email = EmailField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField()
+
+
+class ProductForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(), Length(max=140)])
+    price = FloatField("Price", validators=[
+                       DataRequired(), NumberRange(min=0)])
+    description = TextAreaField("Description")
     submit = SubmitField()
