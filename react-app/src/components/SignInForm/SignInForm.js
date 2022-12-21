7e06e5ca0ff0f3e96d3cd8f3350869cf7dda9e15
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-20 22:37:17 -0600

    Add logo to sign up page

--- react-app/src/components/SignInForm/SignInForm.js
+++ react-app/src/components/SignInForm/SignInForm.js
@@ -2,6 +2,7 @@ import styles from './SignInForm.module.css';
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import * as sessionActions from "../../store/session";
+import { NavLink } from 'react-router-dom';
 
 export default function SignInForm() {
     const dispatch = useDispatch();
@@ -24,42 +25,50 @@ export default function SignInForm() {
     }
 
     return (
-        <form className={styles.signinForm} onSubmit={handleSubmit}>
-            <div className={styles.signinHeader}>
-                <div className={styles.signIn}>Sign in</div>
-            </div>
-            {
-                errors.length > 0 && <ul className={styles.formErrors}>
-                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-                </ul>
-            }
-            <label >
-                Email <br />
-                <input
-                    className="field"
-                    type="text"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-            </label>
-            <label>
-                Password <br />
-                <input
-                    className="field"
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </label>
+        <div className={styles.wrapper} >
+            <NavLink to="/" style={{ textDecoration: 'none' }}>
+                <img src="/images/logo_black.png" alt="logo_black" />
+            </NavLink>
+            <form className={styles.signinForm} onSubmit={handleSubmit}>
 
-            <div className={styles.signindiv} onClick={onClickContinue}>Continue</div>
 
-            <div type="submit" className={styles.demoButton} onClick={() => {
-                setEmail("demo@aa.io");
-                setPassword("password");
-            }}>Log in as demo user</div>
-        </form>
+                <div className={styles.signinHeader}>
+                    <div className={styles.signIn}>Sign in</div>
+                </div>
+                {
+                    errors.length > 0 && <ul className={styles.formErrors}>
+                        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                    </ul>
+                }
+                <label >
+                    Email <br />
+                    <input
+                        className="field"
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </label>
+                <label>
+                    Password <br />
+                    <input
+                        className="field"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </label>
+
+                <div className={styles.signindiv} onClick={onClickContinue}>Continue</div>
+
+                <div type="submit" className={styles.demoButton} onClick={() => {
+                    setEmail("demo@aa.io");
+                    setPassword("password");
+                }}>Log in as demo user</div>
+            </form>
+        </ div>
+
     );
 }
