2026f14c9bb73f40bf71fa50f437e23812a1f564
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-22 12:39:57 -0600

    Add copies of seed to fill grid

--- react-app/src/components/SearchResults/SearchResultItem/SearchResultsItemDescription/SearchResultsItemDescription.js
+++ react-app/src/components/SearchResults/SearchResultItem/SearchResultsItemDescription/SearchResultsItemDescription.js
@@ -5,10 +5,10 @@ export default function SearchResultsItemDescription({ product }) {
     return (
         <div className={styles.wrapper}>
             <div className={styles.title}>{product.title}</div>
-            {product.product_rating &&
+            {product.avg_rating &&
                 <div className={styles.rating}>
-                    <FiveStars rating={product.product_rating} isBlack={true} />
-                    ({product.num_product_ratings})
+                    <FiveStars rating={product.avg_rating} isBlack={true} />
+                    ({product.num_ratings})
                 </div>
             }
             <div className={styles.price}><strong>${parseFloat(product.price).toFixed(2)}</strong></div>
--- react-app/src/components/SearchResults/SearchResults.js
+++ react-app/src/components/SearchResults/SearchResults.js
@@ -1,7 +1,8 @@
+import styles from "./SearchResults.module.css";
+
 import { NavLink, useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 
-import styles from "./SearchResults.module.css";
 import SearchResultsItem from "./SearchResultItem/SearchResultsItem";
 import { useEffect } from "react";
 import { getProductsByKeywords } from "../../store/products";
