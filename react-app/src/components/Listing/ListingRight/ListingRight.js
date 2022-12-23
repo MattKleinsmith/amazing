deeec55b00d47838af40238ea05bce9c47a84eb2
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-23 13:56:09 -0600

    Style ListingRight

--- react-app/src/components/Listing/ListingRight/ListingRight.js
+++ react-app/src/components/Listing/ListingRight/ListingRight.js
@@ -1,52 +1,20 @@
 import styles from "./ListingRight.module.css";
 
-import { useState } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router";
+import Price from "../../Price/Price";
 
 export default function ListingRight({ product }) {
-    const navigate = useNavigate();
-    const user = useSelector(state => state.session.user);
-    const [quantity, setQuantity] = useState(1);
-
     return (
         <div className={styles.wrapper}>
-            <div className={styles.sellerInfo}>
-                <div>{product.seller.fullname}</div>
-            </div>
-            <div className={styles.title}>{product.title}</div>
-            <div className={styles.productPrice}>${(Math.round(product.price * 100) / 100).toFixed(2)}</div>
-            {!user || user.id !== product.seller_id ?
-                <div className={styles.purchaseOptions}>
-                    <label htmlFor="quantity">Quantity</label>
-                    <select
-                        name="quantity"
-                        value={quantity}
-                        onChange={(e) => {
-                            setQuantity(e.target.value);
-                        }}>
-                        {[...Array(11).keys()].slice(1).map((num) => (
-                            <option
-                                key={num}
-                                value={num}
-                            >
-                                {num}</option>))}
-                    </select>
-
-                    <div>
-                        <button className={styles.buyItNow}
-                            onClick={() => { }}
-                        >Buy it now</button>
-                    </div>
-                    <div>
-                        <button className={styles.addToCart}
-                            onClick={() => {
-                            }}>Add to cart</button>
-                    </div>
+            <div className={styles.content}>
+                <div className={styles.price}>
+                    <Price product={product} />
+                    <div className={styles.count}>(${product.price} / Count)</div>
                 </div>
-                :
-                <button onClick={() => navigate(`/your/shop/listing/${product.id}`)}>Edit listing</button>
-            }
+                <div className={`prime ${styles.prime}`} />
+                <div className={styles.freeReturns}>FREE Returns</div>
+                <div className={styles.delivery}>FREE delivery <span className={styles.date}>Tuesday, December 27.</span> Order within <span className={styles.deadline}>10 hrs 13 mins</span></div>
+                <div className={styles.hr} />
+            </div>
         </div>
     );
 }
