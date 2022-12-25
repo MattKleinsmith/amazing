7309ad0681f195740473718959d41b2ed731ed29
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-25 13:38:25 -0600

    Make hover effect persist - ListingImages

--- react-app/src/components/Listing/ListingLeft/ListingImages/ListingImages.js
+++ react-app/src/components/Listing/ListingLeft/ListingImages/ListingImages.js
@@ -4,9 +4,13 @@ import { useState } from "react";
 
 export default function ListingImages({ product }) {
     const [url, setUrl] = useState(product.image_urls[0]);
+    const [thumbnail, setThumbnail] = useState();
 
-    const onMouseEnter = (i) => {
+    const onMouseEnter = (i, e) => {
         setUrl(product.image_urls[i]);
+        if (thumbnail) thumbnail.classList.remove(styles.thumbnailHover);
+        e.target.classList.add(styles.thumbnailHover);
+        setThumbnail(e.target);
     };
 
     return (
@@ -14,7 +18,7 @@ export default function ListingImages({ product }) {
 
             <div className={styles.thumbnailListWrapper}>
                 {product.image_urls.map((url, i) =>
-                    <img className={styles.thumbnail} src={url} alt="thumbnail" onMouseEnter={() => onMouseEnter(i)} key={i} />)}
+                    <img className={styles.thumbnail} src={url} alt="thumbnail" onMouseEnter={(e) => onMouseEnter(i, e)} key={i} />)}
             </div>
 
             <div className={styles.imageWrapper}>
