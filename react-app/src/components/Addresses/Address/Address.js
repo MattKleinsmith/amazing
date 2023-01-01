79a2c3eedeb4e380f6aea79605b1ed4fe60e4ddc
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-31 21:12:44 -0600

    Add backend validation for zipcode and phone

--- react-app/src/components/Addresses/Address/Address.js
+++ react-app/src/components/Addresses/Address/Address.js
@@ -15,10 +15,12 @@ export default function Address({ address }) {
         <div className={styles.wrapper}>
             <div>{address.fullname}</div>
             <div>{address.address}</div>
+            {address.building && <div>{address.building}</div>}
             <div>{address.city}</div>
             <div>{address.state}</div>
             <div>{address.zipcode}</div>
             <div>{address.region}</div>
+            <div>{address.phone}</div>
             <NavLink to={`/addresses/${address.id}`}><button className={styles.button}>Edit</button></NavLink>
             <button onClick={onClickDelete} className={styles.button}>Remove</button>
             <button className={styles.button}>Set as Default</button>
