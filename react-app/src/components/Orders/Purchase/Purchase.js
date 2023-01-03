25de0ea0ec783a9a2cd79d6f4c89aa5827435d26
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2023-01-03 17:56:54 -0600

    Style Buy It Again

--- react-app/src/components/Orders/Purchase/Purchase.js
+++ react-app/src/components/Orders/Purchase/Purchase.js
@@ -1,7 +1,22 @@
 import styles from "./Purchase.module.css";
 
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+
+import { getProductDetails } from "../../../store/productDetails";
+
 export default function Purchase({ purchase }) {
     const createdAt = (new Date(purchase.created_at)).toLocaleDateString('en-us', { year: "numeric", month: "long", day: "numeric" });;
+
+    const dispatch = useDispatch();
+    const product = useSelector(state => state.productDetails)[purchase.product_id];
+
+    useEffect(() => {
+        dispatch(getProductDetails(purchase.product_id));
+    }, [dispatch]);
+
+    if (!product) return;
 
     return (
         <div className={styles.wrapper}>
@@ -28,6 +43,19 @@ export default function Purchase({ purchase }) {
             <div className={styles.bottom}>
                 <div>
                     <div className={styles.status}>Not yet shipped</div>
+                    <div className={styles.product}>
+                        <NavLink to={`/listing/${product.id}`}><img className={styles.previewImage} src={product.preview_image} alt={purchase.order_id} /></NavLink>
+                        <div>
+                            <NavLink to={`/listing/${product.id}`} className={styles.title}>{product.title}</NavLink>
+                            <div className={styles.buttonsRow}>
+                                <div className={styles.buyItAgain}>
+                                    <div className={styles.buyItAgainIcon} />
+                                    <div className={styles.buyItAgainText}>Buy it again</div>
+                                </div>
+                                <div className={styles.viewYourItem}>View your item</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div>
                     <div className={styles.reviewButton}>Write a product review</div>
