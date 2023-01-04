03effdb6967ed00243038c6c9b4b1f86f6492457
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2023-01-04 12:56:38 -0600

    Style footer

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,3 +1,5 @@
+import styles from "./Footer.module.css"
+
 import { useLocation } from "react-router"
 
 export default function Footer() {
@@ -5,5 +7,20 @@ export default function Footer() {
     const excludedPaths = ["signin", "register"];
     if (excludedPaths.some(path => location.pathname.includes(path)))
         return;
-    return "Footer component";
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.footer}>
+                <div className={styles.content}>
+                    <a href="https://www.linkedin.com/in/matthewkleinsmith/" className={styles.link}>
+                        <img className={styles.linkedinIcon} src="/images/linkedin.png" alt="linkedin logo" />
+                    </a>
+                    <a href="https://github.com/MattKleinsmith/" className={styles.link}>
+                        <img className={styles.githubIcon} src="/images/github-logo.png" alt="github logo" />
+                        <img className={styles.githubText} src="/images/github.png" alt="github text logo" />
+                    </a>
+                    <a href="https://github.com/MattKleinsmith/amazing/" className={styles.link}><h2>Project repo</h2></a>
+                </div>
+            </div>
+        </div>
+    );
 }
