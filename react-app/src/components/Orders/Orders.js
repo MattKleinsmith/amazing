440a1abf1e0f593953bff90eabbaceaead37aa76
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2023-01-03 18:25:41 -0600

    Push for pre-grade

--- react-app/src/components/Orders/Orders.js
+++ react-app/src/components/Orders/Orders.js
@@ -6,9 +6,14 @@ import { useEffect } from "react";
 import { getPurchases } from "../../store/purchases"
 
 import Purchase from "./Purchase/Purchase";
+import { useState } from "react";
 
 export default function Orders() {
-    const purchases = useSelector(state => state.purchases);
+    const [page, setPage] = useState(1);
+    const size = 10;
+    const min = (page - 1) * size;
+    const max = min + size;
+    const purchases = useSelector(state => state.purchases).slice(min, max);
 
     const dispatch = useDispatch();
 
@@ -16,15 +21,31 @@ export default function Orders() {
         dispatch(getPurchases());
     }, [dispatch]);
 
+    const incrementPage = () => {
+        if (page * 10 < purchases.length) setPage(page + 1);
+    }
+
+    const decrementPage = () => {
+        if (page > 1) setPage(page - 1);
+    }
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.content}>
+
                 <div className={styles.navInfo}>Your Account {">"} <span className={styles.youAreHere}>Your Orders</span></div>
+
                 <div className={styles.bar}>
                     <div className={styles.heading}>Your Orders</div>
                 </div>
+
                 <div className={styles.purchases}>
                     {purchases.map((purchase, i) => <Purchase key={i} purchase={purchase} />)}
+                </div>
+
+                <div className={styles.paginationBar}>
+                    <div className={styles.paginationButton} onClick={decrementPage}>←Previous</div>
+                    <div className={styles.paginationButton} onClick={incrementPage}>Next→</div>
                 </div>
             </div>
         </div>
