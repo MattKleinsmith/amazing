0423acb9dceb191fb1b9cdaee220e2319c9b2109
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2023-01-06 15:51:07 -0600

    Apply css transition to rating bar

--- react-app/src/components/Listing/ListingReviews/ListingReviews.js
+++ react-app/src/components/Listing/ListingReviews/ListingReviews.js
@@ -1,15 +1,26 @@
 import styles from "./ListingReviews.module.css";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import { getReviewsByProductId } from "../../../store/reviews";
+
 import ListingReviewsBreakdown from "./ListingReviewsBreakdown/ListingReviewsBreakdown";
 import ListingReviewsList from "./ListingReviewsList/ListingReviewsList";
+import { useEffect } from "react";
 
 export default function ListingReviews({ product }) {
-    const reviews = []
+    const dispatch = useDispatch();
 
-    if (!reviews) return;
+    useEffect(() => {
+        dispatch(getReviewsByProductId(product.id));
+    }, [dispatch, product.id]);
+
+    const reviews = useSelector(state => Object.values(state.reviews));
+
     return (
         <div className={styles.wrapper}>
-            <ListingReviewsBreakdown product={product} />
-            <ListingReviewsList product={product} />
+            <ListingReviewsBreakdown product={product} reviews={reviews} />
+            <ListingReviewsList product={product} reviews={reviews} />
         </div>
     );
 }
--- react-app/src/components/Listing/ListingReviews/ListingReviewsList/ListingReviewsList.js
+++ react-app/src/components/Listing/ListingReviews/ListingReviewsList/ListingReviewsList.js
@@ -1,19 +1,7 @@
 // import Stars from "../../../Stars/Stars";
 import styles from "./ListingReviewsList.module.css"
 
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { getReviewsByProductId } from "../../../../store/reviews";
-
-export default function ListingReviewsList({ product }) {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getReviewsByProductId(product.id));
-    }, [dispatch, product.id]);
-
-    const reviews = useSelector(state => Object.values(state.reviews));
-
+export default function ListingReviewsList({ product, reviews }) {
     if (reviews.length === 0 || reviews[0].product_id !== product.id) return;
 
     return (
