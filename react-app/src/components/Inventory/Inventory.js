3e914a094476d895772ee3735799291c0e7a9962
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2023-01-05 18:18:48 -0600

    Add columns to Inventory

--- react-app/src/components/Inventory/Inventory.js
+++ react-app/src/components/Inventory/Inventory.js
@@ -17,17 +17,27 @@ export default function Inventory() {
     return (
         <div className={styles.wrapper}>
             <div className={styles.bar}>
-                <div className={styles.heading}>Manage products</div>
+                <div className={styles.heading}>Manage product listings</div>
                 {products.length !== 0 && <div className={styles.addProductWrapper}>
                     <NavLink to="/inventory/add"><button className={styles.addProduct}>Add product</button></NavLink>
                 </div>}
             </div>
-            <div className={styles.inventory}>
+            {products.length !== 0 && <div className={styles.inventory}>
+                <div className={styles.columnHeaders}>
+                    <div className={styles.image}>Image</div>
+                    <div className={styles.title}>Title</div>
+                    <div className={styles.date}>
+                        <div>Created</div>
+                        <div>Updated</div>
+                    </div>
+                    <div>Price</div>
+                </div>
+                <div className={styles.line} />
                 {products.map((product, i) => <InventoryItem key={i} product={product} />)}
-            </div>
+            </div>}
             {products.length === 0 &&
                 <div className={styles.emptyWrapper}>
-                    <div>You have no products.</div>
+                    <div>You have no product listings.</div>
                     <div className={styles.addProductWrapperEmpty}>
                         <NavLink to="/inventory/add"><button className={styles.addProduct}>Add product</button></NavLink>
                     </div>
