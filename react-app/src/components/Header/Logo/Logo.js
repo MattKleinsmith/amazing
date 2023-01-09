28576568014801ab8c5e0fe696453aad0dcb7bfc
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2023-01-08 18:45:36 -0600

    Add prime to logo

--- react-app/src/components/Header/Logo/Logo.js
+++ react-app/src/components/Header/Logo/Logo.js
@@ -1,5 +1,6 @@
-import { NavLink } from "react-router-dom"
 import styles from "./Logo.module.css"
+
+import { NavLink } from "react-router-dom"
 import { useDispatch } from "react-redux"
 import { setShouldClearSearchBar } from "../../../store/searchbar";
 
@@ -7,7 +8,8 @@ export default function Logo() {
     const dispatch = useDispatch();
     return (
         <NavLink className={styles.wrapper} to="/" style={{ textDecoration: 'none' }} onClick={() => dispatch(setShouldClearSearchBar(""))}>
-            <img src="/images/logo_white.png" alt="logo_white" />
+            <img className={styles.logo} src="/images/logo_white.png" alt="logo_white" />
+            <div className={styles.prime} />
         </NavLink>
     )
 }
