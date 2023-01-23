9958e3d315318e280d2ab5414c143e84fae4444b
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2023-01-23 15:40:07 -0600

    Add more complicated order seed data

--- app/api/purchases.py
+++ app/api/purchases.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_login import login_required, current_user
 
-from app.models import Purchase
+from app.models import Order
 
 bp = Blueprint("purchases", __name__, url_prefix="/purchases")
 
@@ -9,7 +9,7 @@ bp = Blueprint("purchases", __name__, url_prefix="/purchases")
 @bp.route("/current",  methods=["GET"])
 @login_required
 def get_current_user_purchases():
-    purchases = Purchase.query.filter(Purchase.buyer_id == current_user.id)
-    result = [purchase.to_dict() for purchase in purchases]
+    orders = Order.query.filter(Order.buyer_id == current_user.id).all()
+    result = [[purchase.to_dict() for purchase in order.purchases] for order in orders]
     result.reverse()
     return result
