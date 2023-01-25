eb5b35fb0264de0fd6536f15c57c35a71f6511fd
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2023-01-25 16:44:04 -0600

    Style empty Cart page

--- react-app/src/components/Cart/Cart.js
+++ react-app/src/components/Cart/Cart.js
@@ -4,6 +4,8 @@ import { getProductDetails } from "../../store/productDetails";
 
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+
 import CartItem from "./CartItem/CartItem";
 
 export default function Cart() {
@@ -16,6 +18,14 @@ export default function Cart() {
         productIds.forEach(productId => dispatch(getProductDetails(productId)))
         // eslint-disable-next-line
     }, [dispatch, cartItems]);
+
+    if (productIds.length === 0)
+        return <div className={styles.wrapper}>
+            <div className={styles.content}>
+                <div className={styles.heading}>Your Amazing Cart is empty.</div>
+                <NavLink to="/" className={styles.continue}>Continue shopping</NavLink>
+            </div>
+        </div>;
 
     return <div className={styles.wrapper}>
         <div className={styles.content}>
--- react-app/src/components/Cart/CartItem/CartItem.js
+++ react-app/src/components/Cart/CartItem/CartItem.js
@@ -3,9 +3,27 @@ import styles from "./CartItem.module.css";
 import { NavLink } from "react-router-dom";
 
 import Quantity from "./Quantity/Quantity";
+import { useDispatch } from "react-redux";
+import { deleteCartItem } from "../../../store/cartItems";
+import { useState } from "react";
 
 export default function CartItem({ product, quantity }) {
+    const dispatch = useDispatch();
+    const [isDeleted, setIsDeleted] = useState(false);
+
+    const onDelete = () => {
+        dispatch(deleteCartItem(product.id));
+        setIsDeleted(true);
+    };
+
     if (!product) return;
+    if (isDeleted) return <>
+        <div className={styles.deleted}>
+            <NavLink to={`/listing/${product.id}`} className={styles.deletedLink}>
+                {product.title}
+            </NavLink>was removed from Shopping Cart.</div>
+        <div className={styles.line} />
+    </>;
     return <>
         <div className={styles.wrapper}>
             <div className={styles.left}>
@@ -19,13 +37,17 @@ export default function CartItem({ product, quantity }) {
                         <div className={styles.row}>
                             <Quantity productId={product.id} quantity={quantity} />
                             <div className={styles.verticalBar} />
-                            <div className={styles.delete}>Delete</div>
+                            <div className={styles.delete}
+                                onClick={onDelete}
+                            >
+                                Delete
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className={styles.price}>${parseFloat(product.price * quantity).toFixed(2)}</div>
         </div>
-        <div className={styles.line}></div>
+        <div className={styles.line} />
     </>;
 }
