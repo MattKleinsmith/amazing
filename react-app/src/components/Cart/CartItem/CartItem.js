70ec06f14ce5c7aca1860924de3e149e7367bbbb
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2023-01-25 12:25:01 -0600

    Style CartItem

--- react-app/src/components/Cart/CartItem/CartItem.js
+++ react-app/src/components/Cart/CartItem/CartItem.js
@@ -2,8 +2,15 @@ import styles from "./CartItem.module.css";
 
 export default function CartItem({ product, quantity }) {
     if (!product) return;
-    return <div className={styles.wrapper}>
-        {product.title}
-        {quantity}
-    </div>;
+    return <>
+        <div className={styles.wrapper}>
+            <img className={styles.image} src={product.preview_image} alt="" />
+            <div className={styles.middle}>
+                <div lassName={styles.title}>{product.title}</div>
+                <div lassName={styles.quantity}>{quantity}</div>
+            </div>
+            <div className={styles.price}>${parseFloat(product.price).toFixed(2)}</div>
+        </div>
+        <div className={styles.line}></div>
+    </>;
 }
