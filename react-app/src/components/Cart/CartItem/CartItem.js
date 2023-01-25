1b78e3a7c492cc56f5828bead687b29eb1efc5c5
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2023-01-25 14:16:07 -0600

    Multiple price by quantity in CartItem

--- react-app/src/components/Cart/CartItem/CartItem.js
+++ react-app/src/components/Cart/CartItem/CartItem.js
@@ -9,11 +9,19 @@ export default function CartItem({ product, quantity }) {
             <div className={styles.left}>
                 <NavLink to={`/listing/${product.id}`}><img className={styles.image} src={product.preview_image} alt="" /></NavLink>
                 <div className={styles.middle}>
-                    <div className={styles.title}><NavLink className={styles.title} to={`/listing/${product.id}`} >{product.title}</NavLink></div>
-                    <div className={styles.quantity}>{quantity}</div>
+                    <div>
+                        <div className={styles.title}><NavLink className={styles.title} to={`/listing/${product.id}`} >{product.title}</NavLink></div>
+                        <div className={styles.inStock}>In Stock</div>
+                        <div className={styles.shippedFrom}>Shipped from: {product.seller.fullname}</div>
+                        <div className={styles.row}>
+                            <div className={styles.quantity}>{quantity}</div>
+                            <div className={styles.verticalBar} />
+                            <div className={styles.delete}>Delete</div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className={styles.price}>${parseFloat(product.price).toFixed(2)}</div>
+            <div className={styles.price}>${parseFloat(product.price * quantity).toFixed(2)}</div>
         </div>
         <div className={styles.line}></div>
     </>;
