72664473c33a23a852155851a05b7353c885910e
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2023-01-27 16:53:04 -0600

    Define OrderSummary

--- react-app/src/components/Checkout/Checkout.js
+++ react-app/src/components/Checkout/Checkout.js
@@ -10,13 +10,13 @@ import { postOrder } from "../../store/orders";
 import CheckoutItem from "./CheckoutItem/CheckoutItem";
 import AddressSelector from "./AddressSelector/AddressSelector";
 import { setAddressModal } from "../../store/ui";
+import { clearCart } from "../../store/cartItems";
+import OrderSummary from "./OrderSummary/OrderSummary";
 
 export default function Checkout() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const [showTerms1, setShowTerms1] = useState(false);
-    const [showTerms2, setShowTerms2] = useState(false);
     const [showTerms1_2, setShowTerms1_2] = useState(false);
     const [showTerms2_2, setShowTerms2_2] = useState(false);
     const [addressIdx, setAddressIdx] = useState(0);
@@ -49,6 +49,7 @@ export default function Checkout() {
             try {
                 const address = `${addresses[addressIdx].fullname}\n${addresses[addressIdx].address.toUpperCase()}\n${addresses[addressIdx].city.toUpperCase()}, ${addresses[addressIdx].state.toUpperCase()} ${addresses[addressIdx].zipcode.toUpperCase()}\n${addresses[addressIdx].region}`;
                 await dispatch(postOrder({ address, cart: cartItems }));
+                dispatch(clearCart());
                 navigate("/order-confirmation");
             } catch (e) {
                 console.log("Order failed:", e);
@@ -197,48 +198,7 @@ export default function Checkout() {
 
                 </div>
 
-                <div>
-                    <div className={styles.subtotalPane}>
-                        <div className={`${styles.proceed} noselect`} onClick={onPlaceOrder}>{addresses.length > 0 ? "Place your order" : "Add an address"}</div>
-                        <div className={styles.terms}>By placing your order, you agree to Amazing's <NavLink onClick={() => setShowTerms1(true)} className={styles.link}>You Must Hire Me Conditions</NavLink> and <NavLink onClick={() => setShowTerms2(true)} className={styles.link}> Just Kidding Notice</NavLink>.</div>
-                        {showTerms1 && <div className={styles.jokeTerms}>There are no terms, I was just kidding.</div>}
-                        {showTerms2 && <div className={styles.jokeTerms2}>Verily, there are no terms.</div>}
-                        <div className={styles.line2} />
-
-                        <div className={`${styles.stepHeader} ${styles.orderSummaryHeading}`}>Order Summary</div>
-
-                        <div className={styles.row}>
-                            <div>Item{numItems > 1 && "s"} ({numItems}):</div>
-                            <div>${parseFloat(subtotal).toFixed(2)}</div>
-                        </div>
-
-                        <div className={styles.row}>
-                            <div>Shipping & handling:</div>
-                            <div>$0.00</div>
-                        </div>
-
-                        <div className={styles.row}>
-                            <div />
-                            <div className={styles.line3} />
-                        </div>
-
-                        <div className={styles.row}>
-                            <div>Total before tax:</div>
-                            <div>${parseFloat(subtotal).toFixed(2)}</div>
-                        </div>
-
-                        <div className={styles.row}>
-                            <div>Estimated tax to be collected:</div>
-                            <div>${parseFloat(taxes).toFixed(2)}</div>
-                        </div>
-
-                        <div className={styles.line2} />
-                        <div className={styles.totalBottom2}>
-                            <div>Order total:</div>
-                            <div>${parseFloat(total).toFixed(2)}</div>
-                        </div>
-                    </div>
-                </div>
+                <OrderSummary addresses={addresses} onPlaceOrder={onPlaceOrder} numItems={numItems} subtotal={subtotal} taxes={taxes} total={total} />
 
             </div>
 
